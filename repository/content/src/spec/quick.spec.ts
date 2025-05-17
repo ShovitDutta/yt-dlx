926a906792cfb05ed764ff993d6eaf5e99b672ba
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-17 20:12:01 +0530

    yt-dlx (async)

--- repository/content/src/spec/quick.spec.ts
+++ repository/content/src/spec/quick.spec.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 import colors from "colors";
 dotenv.config();
 console.clear();
-YouTubeDLX.Video.Highest({ query: "Dil Darbadar" })
+YouTubeDLX.Video.Highest({ query: "Dil Darbadar", stream: true, metadata: true })
     .on("data", data => console.log(colors.italic.green("@data:"), data))
     .on("error", error => console.error(colors.italic.red("@error:"), error));
