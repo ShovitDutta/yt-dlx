f6b68d17f7f089320ad45adf368f9abd20c7f7c4
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 13:25:08 +0530

    ♥ yt-dlx ♥

--- repository/content/src/routes/Audio_Video/Lowest.ts
+++ repository/content/src/routes/Audio_Video/Lowest.ts
@@ -416,6 +416,7 @@ export default async function AudioVideoLowest({
                 if (showProgress) process.stdout.write("\n");
             });
             instance.run();
+            console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
             return { stream: passthroughStream };
         } else {
             const filenameBase = `yt-dlx_AudioVideoLowest_`;
@@ -445,6 +446,7 @@ export default async function AudioVideoLowest({
                 });
                 instance.run();
             });
+            console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
             return { outputPath };
         }
     } catch (error: any) {
@@ -461,6 +463,5 @@ export default async function AudioVideoLowest({
             throw new Error(unexpectedError);
         }
     } finally {
-        console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
     }
 }
