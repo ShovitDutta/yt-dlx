7c978d5149813bc9a2fff357b08da3e769336832
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 12:21:46 +0530

    ♥ yt-dlx ♥

--- repository/content/vitest.config.ts
+++ repository/content/vitest.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
     test: {
         include: ["src/spec/code/**/*.test.ts"],
         fileParallelism: false,
+        reporters: "verbose",
         environment: "node",
         testTimeout: 60000,
+        bail: 1,
     },
 });
