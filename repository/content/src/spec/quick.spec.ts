e15a9af56a217f16705580d87fda23d75b66fe9f
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 20:26:11 +0530

    ♥ yt-dlx ♥

--- repository/content/src/spec/quick.spec.ts
+++ repository/content/src/spec/quick.spec.ts
@@ -5,6 +5,11 @@ import { locator } from "../utils/locator";
 dotenv.config();
 console.clear();
 (async () => {
-    const paths = await locator();
-    console.log(paths);
+    const query = "test query or url";
+    try {
+        const result = await AudioLowest({ query, output: "./full_downloads_al", useTor: true, verbose: true, filter: "vaporwave", showProgress: true });
+        if ("outputPath" in result) console.log("\nDownload with All Options finished:", result.outputPath);
+    } catch (error) {
+        console.error("\nDownload with All Options Error:", error instanceof Error ? error.message : error);
+    }
 })();
