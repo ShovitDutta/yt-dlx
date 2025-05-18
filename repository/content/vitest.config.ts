9e8dfc6269644d6c3f99116a9fab17938b4b1a41
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 12:08:07 +0530

    ♥ yt-dlx ♥

--- repository/content/vitest.config.ts
+++ repository/content/vitest.config.ts
@@ -1,8 +1,2 @@
 import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-    test: {
-        environment: "node",
-        include: ["spec/**/*.test.ts"],
-    },
-});
+export default defineConfig({ test: { environment: "node", include: ["spec/**/*.test.ts"] } });
