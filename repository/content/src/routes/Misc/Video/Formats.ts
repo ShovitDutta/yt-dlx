d3df484a25055ab75ed4faeec9a2ec6cb72d8ecd
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 13:34:57 +0530

    ♥ yt-dlx ♥

--- repository/content/src/routes/Misc/Video/Formats.ts
+++ repository/content/src/routes/Misc/Video/Formats.ts
@@ -130,6 +130,7 @@ export default async function list_formats({ query, verbose }: z.infer<typeof Zo
             AudioHighDRC: metaBody.AudioHighDRC?.map(item => ({ filesizeP: item.filesizeP, format_note: item.format_note })) || [],
             VideoHighHDR: metaBody.VideoHighHDR?.map(item => ({ filesizeP: item.filesizeP, format_note: item.format_note })) || [],
         };
+        console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
         return { data };
     } catch (error: any) {
         if (error instanceof ZodError) {
@@ -145,6 +146,5 @@ export default async function list_formats({ query, verbose }: z.infer<typeof Zo
             throw new Error(unexpectedError);
         }
     } finally {
-        console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
     }
 }
