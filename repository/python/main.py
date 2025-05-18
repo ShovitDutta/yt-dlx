572418912b32546e5e7bff3ad2b3c180ba7024b4
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 20:19:41 +0530

    ♥ yt-dlx ♥

--- repository/python/main.py
+++ repository/python/main.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import subprocess
+
 def find_venv_python():
     if getattr(sys, "frozen", False):
         base_path = sys._MEIPASS
@@ -15,6 +16,7 @@ def find_venv_python():
         return venv_python_path
     else:
         return None
+
 if __name__ == "__main__":
     venv_python = find_venv_python()
     app_script_name = "logic.py"
