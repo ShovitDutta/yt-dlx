2e1ee83238832bc990be562862f64ca594d4bb4c
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-18 20:26:25 +0530

    ♥ yt-dlx ♥

--- repository/content/src/spec/quick.spec.ts
+++ repository/content/src/spec/quick.spec.ts
@@ -6,7 +6,7 @@ console.clear();
 (async () => {
     const query = "test query or url";
     try {
-        const result = await YouTubeDLX.Audio.Lowest({ query, output: "./full_downloads_al", useTor: true, verbose: true, filter: "vaporwave", showProgress: true });
+        const result = await YouTubeDLX.Audio.Lowest({ query, output: "./output", useTor: true, verbose: true, filter: "vaporwave", showProgress: true });
         if ("outputPath" in result) console.log("\nDownload with All Options finished:", result.outputPath);
     } catch (error) {
         console.error("\nDownload with All Options Error:", error instanceof Error ? error.message : error);
