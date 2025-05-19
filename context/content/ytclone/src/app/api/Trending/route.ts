d98b58ed9d31610156196e5ba76c2d02613e743e
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 17:37:27 +0530

    ♥ yt-dlx ♥

--- context/content/ytclone/src/app/api/Trending/route.ts
+++ context/content/ytclone/src/app/api/Trending/route.ts
@@ -1,18 +1,10 @@
 import YouTubeDLX from "yt-dlx";
 import { NextResponse, NextRequest } from "next/server";
-
 export async function GET(req: NextRequest) {
     try {
         const query = req.nextUrl.searchParams.get("query");
-
-        // Ensure query is provided
-        if (!query) {
-            return NextResponse.json({ error: "Search query is required" }, { status: 400 });
-        }
-
+        if (!query) return NextResponse.json({ error: "Search query is required" }, { status: 400 });
         const decodedQuery = decodeURIComponent(query);
-
-        // Call the YouTubeDLX.Search.Video.Multiple function to search for videos
         const result = await YouTubeDLX.Search.Video.Multiple({ query: decodedQuery });
         return NextResponse.json({ result: result }, { status: 200 });
     } catch (error: any) {
