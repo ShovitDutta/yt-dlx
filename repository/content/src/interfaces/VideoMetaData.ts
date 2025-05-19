5a83f29ba0915c0c9f92f3eac499c736dabfd17b
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 09:49:47 +0530

    ♥ yt-dlx ♥

---
+++ repository/content/src/interfaces/VideoMetaData.ts
@@ -0,0 +1,26 @@
+export interface VideoMetaData {
+    id: string;
+    title: string;
+    channel: string;
+    uploader: string;
+    duration: number;
+    thumbnail: string;
+    age_limit: number;
+    channel_id: string;
+    categories: string[];
+    display_id: string;
+    view_count: number;
+    like_count: number;
+    description: string;
+    channel_url: string;
+    webpage_url: string;
+    live_status: string;
+    upload_date: string;
+    uploader_id: string;
+    original_url: string;
+    uploader_url: string;
+    comment_count: number;
+    duration_string: string;
+    channel_follower_count: number;
+    filename: string;
+}
