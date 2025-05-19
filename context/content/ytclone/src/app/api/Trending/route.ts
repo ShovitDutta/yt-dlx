0f4e93769a1a364a91d2fa17f7e4e2a7c43087e1
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 17:46:04 +0530

    ♥ yt-dlx ♥

--- context/content/ytclone/src/app/api/Trending/route.ts
+++ context/content/ytclone/src/app/api/Trending/route.ts
@@ -4,7 +4,7 @@ export async function GET(req: NextRequest) {
     try {
         const region = req.nextUrl.searchParams.get("region");
         if (!region) return NextResponse.json({ error: "Region is required" }, { status: 400 });
-        const query = `Trending in ${decodeURIComponent(region)}`;
+        const query = `Today Trending in ${decodeURIComponent(region)}`;
         const result = await YouTubeDLX.Search.Video.Multiple({ query: query });
         return NextResponse.json({ result: result }, { status: 200 });
     } catch (error: any) {
