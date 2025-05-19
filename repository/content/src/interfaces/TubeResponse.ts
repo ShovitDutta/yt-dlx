07b2454c919dcaf8fd5017030f538a04d3ab31ed
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 13:45:36 +0530

    ♥ yt-dlx ♥
    ♥ yt-dlx ♥

--- repository/content/src/interfaces/TubeResponse.ts
+++
@@ -1,6 +0,0 @@
-// Suggestion: Add JSDoc comments to the interface and its properties to explain the structure and purpose of the response.
-export default interface TubeResponse<T> {
-    data?: T;
-    message?: string;
-    status: "success" | "error";
-}
