9db4620af51e26986dbaeaf857e7f5c33133044d
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 17:39:35 +0530

    ♥ yt-dlx ♥

--- context/content/ytclone/src/app/api/Trending/route.ts
+++ context/content/ytclone/src/app/api/Trending/route.ts
@@ -1,14 +1,23 @@
-import YouTubeDLX from "yt-dlx";
-import { NextResponse, NextRequest } from "next/server";
+import YouTubeDLX from 'yt-dlx';
+import { NextResponse, NextRequest } from 'next/server';
+
 export async function GET(req: NextRequest) {
-    try {
-        const query = req.nextUrl.searchParams.get("query");
-        if (!query) return NextResponse.json({ error: "Search query is required" }, { status: 400 });
-        const decodedQuery = decodeURIComponent(query);
-        const result = await YouTubeDLX.Search.Video.Multiple({ query: decodedQuery });
-        return NextResponse.json({ result: result }, { status: 200 });
-    } catch (error: any) {
-        console.error(error);
-        return NextResponse.json({ error: error.message }, { status: 500 });
+  try {
+    const region = req.nextUrl.searchParams.get('region');
+
+    // Ensure region is provided
+    if (!region) {
+      return NextResponse.json({ error: 'Region is required' }, { status: 400 });
     }
+
+    const query = `Trending in ${decodeURIComponent(region)}`;
+
+    // Call the YouTubeDLX.Search.Video.Multiple function to search for videos
+    const result = await YouTubeDLX.Search.Video.Multiple({ query: query });
+    return NextResponse.json({ result: result }, { status: 200 });
+
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
