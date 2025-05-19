b2b9c0883760f9a7bfcdc59892584296547bdd47
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 13:52:23 +0530

    ♥ yt-dlx ♥

--- context/content/src/interfaces/sizeFormat.ts
+++
@@ -1,4 +0,0 @@
-// Suggestion: Add JSDoc comments to the interface to explain what the function does and what kind of input it expects. Also, add JSDoc to the parameter and return type.
-export default interface sizeFormat {
-    (filesize: number): string | number;
-}
