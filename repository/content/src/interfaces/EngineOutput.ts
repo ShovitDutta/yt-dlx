575f6f213797f6018efca258fa75421d6c1f5cd4
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 10:09:27 +0530

    ♥ yt-dlx ♥

--- repository/content/src/interfaces/EngineOutput.ts
+++ repository/content/src/interfaces/EngineOutput.ts
@@ -2,6 +2,21 @@ import type VideoInfo from "./VideoInfo";
 import type AudioFormat from "./AudioFormat";
 import type VideoFormat from "./VideoFormat";
 import type ManifestFormat from "./ManifestFormat";
+
+interface Format {
+    filesize?: number;
+    format_note?: string;
+    url?: string;
+    ext?: string;
+    height?: number;
+    width?: number;
+    vcodec?: string;
+    acodec?: string;
+    protocol?: string;
+    vbr?: number;
+    // Add other properties as needed based on the structure in Engine.ts
+}
+
 export default interface EngineOutput {
     metaData: VideoInfo;
     BestAudioLow: AudioFormat;
@@ -18,4 +33,5 @@ export default interface EngineOutput {
     VideoHighHDR: VideoFormat[];
     ManifestLow: ManifestFormat[];
     ManifestHigh: ManifestFormat[];
+    allFormats: Format[]; // Add the new property
 }
