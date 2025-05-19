88f651a851ff9201e2f82bde63a2d7baeff8d776
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 16:41:05 +0530

    ♥ yt-dlx ♥

--- context/content/ytclone/src/app/api/Misc/Video/Related/route.ts
+++ context/content/ytclone/src/app/api/Misc/Video/Related/route.ts
@@ -10,8 +10,10 @@ export async function GET(req: NextRequest) {
       return NextResponse.json({ error: 'Video ID is required' }, { status: 400 });
     }
 
+    const decodedVideoId = decodeURIComponent(videoId);
+
     // Call the YouTubeDLX.Misc.Video.Related function to get related videos
-    const result = await YouTubeDLX.Misc.Video.Related({ videoId: videoId });
+    const result = await YouTubeDLX.Misc.Video.Related({ videoId: decodedVideoId });
     return NextResponse.json({ result: result }, { status: 200 });
 
   } catch (error: any) {
