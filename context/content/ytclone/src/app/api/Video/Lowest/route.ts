864002a6f8766b45008a80d599b439c136a1da51
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 16:37:09 +0530

    ♥ yt-dlx ♥

--- context/content/ytclone/src/app/api/Video/Lowest/route.ts
+++ context/content/ytclone/src/app/api/Video/Lowest/route.ts
@@ -10,8 +10,10 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'Video URL is required' }, { status: 400 });
     }
 
+    const decodedVideoUrl = decodeURIComponent(videoUrl);
+
     // Call the YouTubeDLX.Video.Lowest function to download the video
-    const result = await YouTubeDLX.Video.Lowest({ query: videoUrl, ...options });
+    const result = await YouTubeDLX.Video.Lowest({ query: decodedVideoUrl, ...options });
     return NextResponse.json({ result: result }, { status: 200 });
 
   } catch (error: any) {
