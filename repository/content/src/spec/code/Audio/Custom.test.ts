6025a41f4ab611ceea8d08a720b26c5e3c33d600
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-19 11:02:28 +0530

    ♥ yt-dlx ♥

--- repository/content/src/spec/code/Audio/Custom.test.ts
+++ repository/content/src/spec/code/Audio/Custom.test.ts
@@ -3,9 +3,9 @@ import { createWriteStream } from "fs";
 import { Readable } from "stream";
 import * as vitest from "vitest";
 vitest.describe("AudioCustom", () => {
-    const query = "https://youtu.be/dQw4w9WgXcQ";
+    const query = "https://www.youtube.com/watch?v=fp7bbq813Jc";
     vitest.it("should handle basic download", async () => {
-        const result = await AudioCustom({ query, resolution: "high" });
+        const result = await AudioCustom({ query, resolution: "720p60" });
         vitest.expect(result).toHaveProperty("outputPath");
         if ("outputPath" in result) {
             vitest.expect(result.outputPath).toMatch(/\.avi$/);
