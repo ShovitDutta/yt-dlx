8083c7a22eb8e8bcb382053d00c8603c8b218eaa
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 09:28:58 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/app/(root)/page.tsx
+++ context/content/webdl/src/app/(root)/page.tsx
@@ -138,6 +138,7 @@ const VideoCard = memo(({ video }: { video: VideoType }) => {
                                     transition={{ duration: 0.3 }}
                                     className="absolute inset-0 flex items-center justify-center">
                                     <FaPlayCircle className="text-red-800 text-6xl" />
+                                    Watch It Now!
                                 </motion.div>
                             )}
                         </Fragment>
