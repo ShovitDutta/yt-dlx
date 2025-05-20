a477318c024cb3ace30190a0ded8575de736cbe0
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 00:26:36 +0530

    YouTubeDLX

--- context/content/src/utils/Agent.ts
+++ context/content/src/utils/Agent.ts
@@ -1,4 +1,3 @@
-// Suggestion: Add JSDoc comments to the functions, explaining their purpose, parameters, and return values. Also, the interfaces could benefit from JSDoc comments on their properties. Consider using more descriptive names for the interfaces and functions to improve code readability.
 import colors from "colors";
 import Engine from "./Engine";
 import { Client } from "youtubei";
