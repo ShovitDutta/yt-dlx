cb983d41ac81a5fc998868625281e9f510a4fb59
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 07:53:22 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/store/root.ts
+++ context/content/webdl/src/store/root.ts
@@ -1,1 +1,22 @@
 import { create } from "zustand";
+import { proxy } from "valtio";
+
+// Zustand Store
+interface ZustandStore {
+  zustandData: string;
+  setZustandData: (data: string) => void;
+}
+
+export const useZustandStore = create<ZustandStore>((set) => ({
+  zustandData: "",
+  setZustandData: (data) => set({ zustandData: data }),
+}));
+
+// Valtio Store
+interface ValtioStore {
+  valtioData: string;
+}
+
+export const valtioStore = proxy<ValtioStore>({
+  valtioData: "",
+});
