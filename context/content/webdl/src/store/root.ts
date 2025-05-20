ca5d70c9878c32f33bdc31711e3cdec48c7e6963
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 07:55:48 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/store/root.ts
+++ context/content/webdl/src/store/root.ts
@@ -1,10 +1,29 @@
 import { create } from "zustand";
 import { proxy } from "valtio";
 
+interface VideoType {
+    type: string;
+    title: string;
+    videoId: string;
+    authorId: string;
+    thumbnails: any[];
+    authorUrl: string;
+    viewCount: string;
+    authorName: string;
+    description: string;
+    authorBadges: any[];
+    shortViewCount: string;
+    authorThumbnails: any[];
+}
+
 // Zustand Store
 interface ZustandStore {
     zustandData: string;
     setZustandData: (data: string) => void;
+    sectionVideos: { [key: string]: VideoType[] };
+    setSectionVideos: (videos: { [key: string]: VideoType[] }) => void;
+    sectionsLoading: { [key: string]: boolean };
+    setSectionsLoading: (loading: { [key: string]: boolean }) => void;
 }
 
 export const useZustandStore = create<ZustandStore>(set => ({
