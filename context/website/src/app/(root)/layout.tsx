8fb815665a5df8848505a1ae75a7432cb951bb60
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 19:40:34 +0530

    YouTubeDLX
    YouTubeDLX

--- context/website/src/app/(root)/layout.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(root)/layout.tsx
-import React from "react";
-import Providers from "@/app/(root)/providers";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return <Providers>{children}</Providers>;
-}
