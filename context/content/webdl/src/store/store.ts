ed5b0f534d80c048ddacbc66c09505755c639b07
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 07:52:09 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-interface MainStore {
-    data: any;
-    setData: (data: any) => void;
-}
-export const mainStore = create<MainStore>(set => ({ data: null, setData: data => set({ data }) }));
