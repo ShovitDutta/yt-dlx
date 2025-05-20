403953307391455f26af8f8adde61decb6916531
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 07:55:45 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/store/root.ts
+++ context/content/webdl/src/store/root.ts
@@ -3,20 +3,29 @@ import { proxy } from "valtio";
 
 // Zustand Store
 interface ZustandStore {
-  zustandData: string;
-  setZustandData: (data: string) => void;
+    zustandData: string;
+    setZustandData: (data: string) => void;
 }
 
-export const useZustandStore = create<ZustandStore>((set) => ({
-  zustandData: "",
-  setZustandData: (data) => set({ zustandData: data }),
+export const useZustandStore = create<ZustandStore>(set => ({
+    zustandData: "",
+    setZustandData: data => set({ zustandData: data }),
+
+    sectionVideos: {},
+    setSectionVideos: videos => set({ sectionVideos: videos }),
+    sectionsLoading: {},
+    setSectionsLoading: loading => set({ sectionsLoading: loading }),
 }));
 
 // Valtio Store
 interface ValtioStore {
-  valtioData: string;
+    valtioData: string;
+    sectionVideos: { [key: string]: any[] };
+    sectionsLoading: { [key: string]: boolean };
 }
 
 export const valtioStore = proxy<ValtioStore>({
-  valtioData: "",
+    valtioData: "",
+    sectionVideos: {},
+    sectionsLoading: {},
 });
