cb87a0f10dd8e77dd1ae09814bd6512aa7083197
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-20 07:47:30 +0530

    ♥ yt-dlx ♥

--- context/content/webdl/src/app/(root)/layout.tsx
+++ context/content/webdl/src/app/(root)/layout.tsx
@@ -1,5 +1,21 @@
-import { mainStore } from '../../store';
+import { mainStore } from "../../store";
 import React from "react";
+import { useEffect } from "react";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/api/Trending");
+                const data = await response.json();
+                mainStore.setData(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return <>{children}</>;
 }
