ba640b8ae97b3ffbc6d40f8d9ac16127d12a5336
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 14:38:47 +0530

    YouTubeDLX

--- context/content/src/utils/Engine.ts
+++ context/content/src/utils/Engine.ts
@@ -4,6 +4,9 @@ import { locator } from "./Locator";
 import * as readline from "readline";
 import * as retry from "async-retry";
 import type { Format, Entry, EngineOutput, Thumbnail } from "../interfaces";
+import type { AudioFormat } from "../interfaces/AudioFormat";
+import type { VideoFormat } from "../interfaces/VideoFormat";
+import type { ManifestFormat } from "../interfaces/ManifestFormat";
 import { spawn, execFile, ChildProcessWithoutNullStreams } from "child_process";
 let cachedLocatedPaths: Record<string, string> | null = null;
 export const getLocatedPaths = async (): Promise<Record<string, string>> => {
@@ -317,4 +320,57 @@ export default async function Engine(options: {
         },
     };
     return payLoad;
+}
+function MapAudioFormat(i: Format): AudioFormat {
+    return {
+        filesize: i.filesize,
+        asr: i.asr,
+        format_note: i.format_note,
+        tbr: i.tbr,
+        url: i.url,
+        ext: i.ext,
+        acodec: i.acodec,
+        container: i.container,
+        resolution: i.resolution,
+        audio_ext: i.audio_ext,
+        abr: i.abr,
+        format: i.format,
+    };
+}
+function MapVideoFormat(i: Format): VideoFormat {
+    return {
+        fps: i.fps,
+        tbr: i.tbr,
+        url: i.url,
+        ext: i.ext,
+        vbr: i.vbr,
+        width: i.width,
+        format: i.format,
+        height: i.height,
+        vcodec: i.vcodec,
+        filesize: i.filesize,
+        video_ext: i.video_ext,
+        container: i.container,
+        resolution: i.resolution,
+        format_note: i.format_note,
+        aspect_ratio: i.aspect_ratio,
+        dynamic_range: i.dynamic_range,
+    };
+}
+function MapManifest(i: Format): ManifestFormat {
+    return {
+        url: i.url,
+        manifest_url: i.manifest_url,
+        tbr: i.tbr,
+        ext: i.ext,
+        fps: i.fps,
+        width: i.width,
+        height: i.height,
+        vcodec: i.vcodec,
+        dynamic_range: i.dynamic_range,
+        aspect_ratio: i.aspect_ratio,
+        video_ext: i.video_ext,
+        vbr: i.vbr,
+        format: i.format,
+    };
 }
