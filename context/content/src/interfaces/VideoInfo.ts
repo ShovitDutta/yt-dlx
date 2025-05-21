9939febf51fa5f4598fba4770839d94b3c60b9e0
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:35:43 +0530

    YouTubeDLX

--- context/content/src/interfaces/VideoInfo.ts
+++ context/content/src/interfaces/VideoInfo.ts
@@ -1,28 +1,37 @@
+import { Thumbnail } from ".";
+
+export interface ThumbnailInfo {
+    Highest: Thumbnail | null;
+    Lowest: Thumbnail | null;
+    Combined: Thumbnail[];
+}
+
 export type VideoInfo = Pick<
     import(".").Entry,
-    | "id"
-    | "title"
-    | "channel"
-    | "uploader"
-    | "duration"
-    | "thumbnails"
-    | "like_count"
-    | "view_count"
-    | "age_limit"
-    | "channel_id"
-    | "categories"
-    | "display_id"
-    | "description"
-    | "channel_url"
-    | "webpage_url"
-    | "live_status"
-    | "upload_date"
-    | "uploader_id"
-    | "original_url"
-    | "uploader_url"
-    | "comment_count"
-    | "view_count"
-    | "like_count"
-    | "duration_string"
-    | "channel_follower_count"
+"id"
+"title"
+"channel"
+"uploader"
+"duration"
+"age_limit"
+"channel_id"
+"categories"
+"display_id"
+"description"
+"channel_url"
+"webpage_url"
+"live_status"
+"upload_date"
+"uploader_id"
+"original_url"
+"uploader_url"
+"comment_count"
+"view_count"
+"like_count"
+"duration_string"
+"channel_follower_count"
 >;
+
+export interface VideoInfo {
+    thumbnails: ThumbnailInfo;
+}
