08b875d5e30ad0fc6a4aa9094c2cbea95d134cb7
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 14:07:04 +0530

    YouTubeDLX

--- context/content/src/routes/Video/Custom.ts
+++ context/content/src/routes/Video/Custom.ts
@@ -97,7 +97,7 @@ export default async function VideoCustom({
             }
             instance.setFfmpegPath(paths.ffmpeg);
             instance.setFfprobePath(paths.ffprobe);
-            if (EngineMeta.MetaData.thumbnails?.Highest?.url) {
+            if (EngineMeta.MetaData.thumbnails.Highest) {
                 instance.addInput(EngineMeta.MetaData.thumbnails.Highest.url);
             }
         } catch (locatorError: any) {
