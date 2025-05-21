732a83372c6c656ca413c3267b518b32adf51a9c
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:21:47 +0530

    YouTubeDLX

--- context/content/src/interfaces/EngineOutput.ts
+++ context/content/src/interfaces/EngineOutput.ts
@@ -2,17 +2,12 @@ import type { VideoInfo } from "./VideoInfo";
 import type { AudioFormat } from "./AudioFormat";
 import type { VideoFormat } from "./VideoFormat";
 import type { ManifestFormat } from "./ManifestFormat";
-
 export interface EngineOutput {
     MetaData: VideoInfo;
     AvailableFormats: {
-        Audio: string[];
-        Video: string[];
-        Manifest: {
-            // Changed to an object
-            Audio: string[]; // Array of strings for audio-only manifests
-            Video: string[]; // Array of strings for video manifests
-        };
+        Audio: AudioFormat[];
+        Video: VideoFormat[];
+        Manifest: ManifestFormat[];
     };
     Audio: {
         SingleQuality: {
