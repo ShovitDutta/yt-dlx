0c2ad2ee538165dcb5f9b81a9e67f755759d4b59
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:11:07 +0530

    YouTubeDLX

--- context/content/src/utils/Engine.ts
+++ context/content/src/utils/Engine.ts
@@ -205,16 +205,7 @@ export default async function Engine(options: {
             }
         } else if (isAudio) {
             const mappedAudio = MapAudioFormat(tube);
-            delete mappedAudio.fps;
-            delete mappedAudio.width;
-            delete mappedAudio.height;
-            delete mappedAudio.vcodec;
-            delete mappedAudio.dynamic_range;
-            delete mappedAudio.aspect_ratio;
-            delete mappedAudio.video_ext;
-            delete mappedAudio.vbr;
-            delete mappedAudio.url;
-            delete mappedAudio.manifest_url;
+            
             AvailableParsedAudioFormats.push(mappedAudio);
             if (isDRC) {
                 if (!audioHasDRC.Lowest || (mappedAudio.filesize !== undefined && audioHasDRC.Lowest[0]?.filesize !== undefined && mappedAudio.filesize < audioHasDRC.Lowest[0].filesize)) {
