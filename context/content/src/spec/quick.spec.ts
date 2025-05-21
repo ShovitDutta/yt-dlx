108e28446bbbd1f1fa95ad0b32d3417ed781ffe1
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 12:39:18 +0530

    YouTubeDLX

--- context/content/src/spec/quick.spec.ts
+++ context/content/src/spec/quick.spec.ts
@@ -8,4 +8,4 @@ console.clear();
     const result = await YouTubeDLX.Misc.Video.Extract({ query: "https://www.youtube.com/watch?v=fp7bbq813Jc" });
     fs.writeFileSync("quick.json", JSON.stringify(result, null, 2));
 
-})
+})()
