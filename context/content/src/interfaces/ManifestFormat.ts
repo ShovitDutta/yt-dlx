b1e6ceeb75d0bcb069f5b58c82e8e970c2fab3b1
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:10:26 +0530

    YouTubeDLX

--- context/content/src/interfaces/ManifestFormat.ts
+++ context/content/src/interfaces/ManifestFormat.ts
@@ -1,14 +1,4 @@
 export type ManifestFormat = Pick<
     import(".").Format,
-    "url" | "manifest_url" | "tbr" | "ext" | "fps" | "width" | "height" | "vcodec" | "dynamic_range" | "aspect_ratio" | "video_ext" | "vbr" | "format"> & {
-    fps?: number;
-    width?: number;
-    height?: number;
-    vcodec?: string;
-    dynamic_range?: string;
-    aspect_ratio?: number;
-    video_ext?: string;
-    vbr?: number;
-    url?: string;
-    manifest_url?: string;
-};
+    "url" | "manifest_url" | "tbr" | "ext" | "fps" | "width" | "height" | "vcodec" | "dynamic_range" | "aspect_ratio" | "video_ext" | "vbr" | "format"
+>;
