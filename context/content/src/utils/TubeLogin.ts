7003759d60164b4c4cc0ba458b3594b15e457703
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 12:48:19 +0530

    YouTubeDLX

--- context/content/src/utils/TubeLogin.ts
+++ context/content/src/utils/TubeLogin.ts
@@ -4,18 +4,16 @@ import colors from "colors";
 import { Innertube, UniversalCache } from "youtubei.js";
 export type TubeType = Innertube;
 export let Tube: TubeType | null = null;
-export default async function TubeLogin(cookiesFilePathOrString?: string): Promise<TubeType> {
-    let cookiesData: string | undefined;
-    if (cookiesFilePathOrString) {
-        if (fs.existsSync(cookiesFilePathOrString)) {
-            try {
-                cookiesData = fs.readFileSync(cookiesFilePathOrString, "utf8");
-            } catch (error) {
-                console.error(colors.red("@error:"), "Failed to read cookies file.");
-                process.exit(1);
-            }
-        } else cookiesData = cookiesFilePathOrString;
-    }
+export default async function TubeLogin(cookiesFilePathOrString: string): Promise<TubeType> {
+    let cookiesData: string;
+    if (fs.existsSync(cookiesFilePathOrString)) {
+        try {
+            cookiesData = fs.readFileSync(cookiesFilePathOrString, "utf8");
+        } catch (error) {
+            console.error(colors.red("@error:"), "Failed to read cookies file.");
+            process.exit(1);
+        }
+    } else cookiesData = cookiesFilePathOrString;
     try {
         Tube = await Innertube.create({
             user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
