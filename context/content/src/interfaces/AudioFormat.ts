d66d212bcb15726da621e3130d1b6c9e72520fb6
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:06:58 +0530

    YouTubeDLX

--- context/content/src/interfaces/AudioFormat.ts
+++ context/content/src/interfaces/AudioFormat.ts
@@ -1,5 +1,14 @@
 export interface AudioFormat {
-    format: string;
-    format_note: string;
     filesize?: number;
+    asr?: number;
+    format_note: string;
+    tbr: number | null;
+    url: string;
+    ext: string;
+    acodec: string;
+    container?: string;
+    resolution?: string | null;
+    audio_ext?: string;
+    abr?: number | null;
+    format: string;
 }
--- context/content/src/interfaces/VideoFormat.ts
+++ context/content/src/interfaces/VideoFormat.ts
@@ -1,5 +1,4 @@
-export interface VideoFormat {
-    format: string;
-    format_note: string;
-    filesize?: number;
-}
+export type VideoFormat = Pick<
+    import(".").Format,
+    "filesize" | "format_note" | "fps" | "height" | "width" | "tbr" | "url" | "ext" | "vcodec" | "dynamic_range" | "container" | "resolution" | "aspect_ratio" | "video_ext" | "vbr" | "format"
+> & { filesizeP?: string | number | null };
