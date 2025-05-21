37a9d2b837d7de72212564aaa56b138502f57737
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 14:41:29 +0530

    YouTubeDLX

--- context/content/src/utils/Engine.ts
+++ context/content/src/utils/Engine.ts
@@ -4,10 +4,10 @@ import { locator } from "./Locator";
 import * as readline from "readline";
 import * as retry from "async-retry";
 import type { Format, Entry, EngineOutput, Thumbnail } from "../interfaces";
-import { spawn, execFile, ChildProcessWithoutNullStreams } from "child_process";
 import type { AudioFormat } from "../interfaces/AudioFormat";
 import type { VideoFormat } from "../interfaces/VideoFormat";
 import type { ManifestFormat } from "../interfaces/ManifestFormat";
+import { spawn, execFile, ChildProcessWithoutNullStreams } from "child_process";
 let cachedLocatedPaths: Record<string, string> | null = null;
 export const getLocatedPaths = async (): Promise<Record<string, string>> => {
     if (cachedLocatedPaths === null) cachedLocatedPaths = await locator();
@@ -306,18 +306,70 @@ export default async function Engine(options: {
         },
         Audio: {
             HasDRC: audioHasDRC.Lowest || audioHasDRC.Highest ? audioHasDRC : undefined,
-            SingleQuality: { Lowest: audioSingleQuality.Lowest ?? undefined, Highest: audioSingleQuality.Highest ?? undefined },
+            SingleQuality: { Lowest: audioSingleQuality.Lowest ?? null, Highest: audioSingleQuality.Highest ?? null },
             MultipleQuality: { Lowest: audioMultipleQuality.Lowest, Highest: audioMultipleQuality.Highest },
         },
         Video: {
             HasHDR: videoHasHDR.Lowest || videoHasHDR.Highest ? videoHasHDR : undefined,
-            SingleQuality: { Lowest: videoSingleQuality.Lowest ?? undefined, Highest: videoSingleQuality.Highest ?? undefined },
-            MultipleQuality: { Lowest: videoMultipleQuality.Lowest, Highest: videoMultipleQuality.Highest },
+            SingleQuality: { Lowest: videoSingleQuality.Lowest ?? null, Highest: videoSingleQuality.Highest ?? null },
         },
         Manifest: {
-            SingleQuality: { Lowest: manifestSingleQuality.Lowest ?? undefined, Highest: manifestSingleQuality.Highest ?? undefined },
+            SingleQuality: { Lowest: manifestSingleQuality.Lowest ?? null, Highest: manifestSingleQuality.Highest ?? null },
             MultipleQuality: { Lowest: manifestMultipleQuality.Lowest, Highest: manifestMultipleQuality.Highest },
         },
     };
+    function MapAudioFormat(i: Format): AudioFormat {
+        return {
+            filesize: i.filesize,
+            asr: i.asr,
+            format_note: i.format_note,
+            tbr: i.tbr,
+            url: i.url,
+            ext: i.ext,
+            acodec: i.acodec,
+            container: i.container,
+            resolution: i.resolution,
+            audio_ext: i.audio_ext,
+            abr: i.abr,
+            format: i.format,
+        };
+    }
+    function MapVideoFormat(i: Format): VideoFormat {
+        return {
+            fps: i.fps,
+            tbr: i.tbr,
+            url: i.url,
+            ext: i.ext,
+            vbr: i.vbr,
+            width: i.width,
+            format: i.format,
+            height: i.height,
+            vcodec: i.vcodec,
+            filesize: i.filesize,
+            video_ext: i.video_ext,
+            container: i.container,
+            resolution: i.resolution,
+            format_note: i.format_note,
+            aspect_ratio: i.aspect_ratio,
+            dynamic_range: i.dynamic_range,
+        };
+    }
+    function MapManifest(i: Format): ManifestFormat {
+        return {
+            url: i.url,
+            manifest_url: i.manifest_url,
+            tbr: i.tbr,
+            ext: i.ext,
+            fps: i.fps,
+            width: i.width,
+            height: i.height,
+            vcodec: i.vcodec,
+            dynamic_range: i.dynamic_range,
+            aspect_ratio: i.aspect_ratio,
+            video_ext: i.video_ext,
+            vbr: i.vbr,
+            format: i.format,
+        };
+    }
     return payLoad;
 }
