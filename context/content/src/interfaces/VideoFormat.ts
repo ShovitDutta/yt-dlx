cde02255c1fe72ca113901f1f87f18d5d8740406
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 13:06:45 +0530

    YouTubeDLX

--- context/content/src/interfaces/VideoFormat.ts
+++ context/content/src/interfaces/VideoFormat.ts
@@ -1,4 +1,5 @@
-export type VideoFormat = Pick<
-    import(".").Format,
-    "filesize" | "format_note" | "fps" | "height" | "width" | "tbr" | "url" | "ext" | "vcodec" | "dynamic_range" | "container" | "resolution" | "aspect_ratio" | "video_ext" | "vbr" | "format"
-> & { filesizeP?: string | number | null };
+export interface VideoFormat {
+    format: string;
+    format_note: string;
+    filesize?: number;
+}
