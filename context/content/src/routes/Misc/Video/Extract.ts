c6c767f2081332b73e8a5020c52e06608b3251bf
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 12:54:46 +0530

    YouTubeDLX

--- context/content/src/routes/Misc/Video/Extract.ts
+++ context/content/src/routes/Misc/Video/Extract.ts
@@ -231,7 +231,7 @@ export default async function extract(options: z.infer<typeof ZodSchema>): Promi
             comments,
             transcript,
         };
-        return payload;
+        return { data: payload };
     } catch (error: any) {
         if (error instanceof ZodError) {
             const errorMessage = `${colors.red("@error:")} Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`;
