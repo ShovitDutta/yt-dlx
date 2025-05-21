ad99b657bf85f8eed48a05fcc43988b6e484ee50
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 12:39:14 +0530

    YouTubeDLX

--- context/content/src/spec/quick.spec.ts
+++ context/content/src/spec/quick.spec.ts
@@ -1,17 +1,11 @@
-import * as vitest from "vitest";
 import dotenv from "dotenv";
 import YouTubeDLX from "..";
 import fs from "fs";
 dotenv.config();
 console.clear();
 
-vitest.it("should extract video data and save to json", async () => {
-    try {
-        const result = await YouTubeDLX.Misc.Video.Extract({ query: "https://www.youtube.com/watch?v=fp7bbq813Jc" });
-        fs.writeFileSync("quick.json", JSON.stringify(result, null, 2));
-        vitest.expect(result).toHaveProperty("data");
-    } catch (error) {
-        console.error("Failed to extract video data", error);
-        throw error;
-    }
-});
+(async () => {
+    const result = await YouTubeDLX.Misc.Video.Extract({ query: "https://www.youtube.com/watch?v=fp7bbq813Jc" });
+    fs.writeFileSync("quick.json", JSON.stringify(result, null, 2));
+
+})
