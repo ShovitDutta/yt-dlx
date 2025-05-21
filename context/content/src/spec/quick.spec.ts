6132c01f6815217a763854fbe18fa778f696dbb6
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 12:50:00 +0530

    YouTubeDLX

--- context/content/src/spec/quick.spec.ts
+++ context/content/src/spec/quick.spec.ts
@@ -1,18 +1,9 @@
-import extract from "../routes/Misc/Video/Extract";
-import fs from "fs";
 import dotenv from "dotenv";
+import YouTubeDLX from "..";
+import fs from "fs";
 dotenv.config();
 console.clear();
-
-async function quickSpec() {
-    const query = "https://www.youtube.com/watch?v=fp7bbq813Jc";
-    try {
-        const result = await extract({ query });
-        fs.writeFileSync("quick.json", JSON.stringify(result, null, 2));
-        console.log("Extracted data and saved to quick.json");
-    } catch (error) {
-        console.error("Failed to extract video data", error);
-    }
-}
-
-quickSpec();
+(async () => {
+    const result = await YouTubeDLX.Misc.Video.Extract({ query: "https://www.youtube.com/watch?v=fp7bbq813Jc" });
+    fs.writeFileSync("quick.json", JSON.stringify(result, null, 2));
+})().catch(console.error);
