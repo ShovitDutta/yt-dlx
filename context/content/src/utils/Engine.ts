68a815d477cdfe94f3ff1a88c13111f41b7e75f3
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-21 14:03:21 +0530

    YouTubeDLX

--- context/content/src/utils/Engine.ts
+++ context/content/src/utils/Engine.ts
@@ -271,17 +271,9 @@ export default async function Engine(options: {
             uploader: i.uploader,
             duration: i.duration,
             thumbnails: {
-                Highest: i.thumbnails?.Combined
-                    ? i.thumbnails.Combined.reduce((prev, curr) => (prev.preference > curr.preference ? prev : curr))
-                    : i.thumbnails?.high
-                        ? i.thumbnails.high.reduce((prev, curr) => (prev.preference > curr.preference ? prev : curr))
-                        : null,
-                Lowest: i.thumbnails?.Combined
-                    ? i.thumbnails.Combined.reduce((prev, curr) => (prev.preference < curr.preference ? prev : curr))
-                    : i.thumbnails?.low
-                        ? i.thumbnails.low.reduce((prev, curr) => (prev.preference < curr.preference ? prev : curr))
-                        : null,
-                Combined: i.thumbnails?.Combined ?? (i.thumbnails?.default ? i.thumbnails.default : []),
+                Combined: Object.values(i.thumbnails || {}).filter(thumbnail => thumbnail && typeof thumbnail === 'object' && 'url' in thumbnail) as any,
+                Highest: Object.values(i.thumbnails || {}).filter(thumbnail => thumbnail && typeof thumbnail === 'object' && 'url' in thumbnail).reduce((prev: any, curr: any) => ((prev?.width * prev?.height) > (curr?.width * curr?.height) ? prev : curr), null) as any,
+                Lowest: Object.values(i.thumbnails || {}).filter(thumbnail => thumbnail && typeof thumbnail === 'object' && 'url' in thumbnail).reduce((prev: any, curr: any) => ((prev?.width * prev?.height) < (curr?.width * curr?.height) ? prev : curr), null) as any,
             },
             age_limit: i.age_limit,
             channel_id: i.channel_id,
