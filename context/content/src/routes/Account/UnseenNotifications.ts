50a460601ead433332ed1cb899c2f7f642b2496e
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:59:58 +0530

    YouTubeDLX

--- context/content/src/routes/Account/UnseenNotifications.ts
+++ context/content/src/routes/Account/UnseenNotifications.ts
@@ -18,19 +18,10 @@ export default async function unseen_notifications(options: UnseenNotificationsO
         if (count === undefined) throw new Error(`${colors.red("@error:")} Failed to fetch unseen notifications count.`);
         const result: TubeResponse<{ count: number }> = { status: "success", data: { count: Number(count) || 0 } };
         return result;
-    } catch (error: any) {
-        if (error instanceof ZodError) {
-            const errorMessage = `${colors.red("@error:")} Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`;
-            console.error(errorMessage);
-            throw new Error(errorMessage);
-        } else if (error instanceof Error) {
-            console.error(error.message);
-            throw error;
-        } else {
-            const unexpectedError = `${colors.red("@error:")} An unexpected error occurred: ${String(error)}`;
-            console.error(unexpectedError);
-            throw new Error(unexpectedError);
-        }
+    } catch (error) {
+        if (error instanceof ZodError) throw new Error(`${colors.red("@error:")} Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`);
+        else if (error instanceof Error) throw error;
+        else throw new Error(`${colors.red("@error:")} An unexpected error occurred: ${String(error)}`);
     } finally {
         if (Verbose) console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
     }
