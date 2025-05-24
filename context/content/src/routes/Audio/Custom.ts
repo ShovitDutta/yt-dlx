76fba64a3bc4c909f56caba1ab049637cbe96543
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:42:51 +0530

    YouTubeDLX

--- context/content/src/routes/Audio/Custom.ts
+++ context/content/src/routes/Audio/Custom.ts
@@ -47,7 +47,6 @@ export default async function AudioCustom({
             throw new Error(`${colors.red("@error:")} The 'MetaData' parameter cannot be used with other processing parameters.`);
         }
         if (Stream && Output) throw new Error(`${colors.red("@error:")} The 'Stream' parameter cannot be used with 'Output'.`);
-
         if (AudioFormatId && AudioBitrate) throw new Error(`${colors.red("@error:")} The 'AudioFormatId' and 'AudioBitrate' parameters cannot be used together. Please specify only one.`);
 
         const EngineMeta: EngineOutput | null = await Agent({ Query: Query, Verbose: Verbose, UseTor: UseTor });
