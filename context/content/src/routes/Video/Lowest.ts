71af88e7f6ba6470de72c3e7eac80673facd10ac
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:57:09 +0530

    YouTubeDLX

--- context/content/src/routes/Video/Lowest.ts
+++ context/content/src/routes/Video/Lowest.ts
@@ -35,20 +35,16 @@ export default async function VideoLowest({
     try {
         ZodSchema.parse({ Query, Output, UseTor, Stream, Filter, MetaData, Verbose, ShowProgress });
 
-        if (MetaData && (Stream || Output || Filter || ShowProgress))
+        if (MetaData && (Stream || Output || Filter || ShowProgress)) {
             throw new Error(`${colors.red("@error:")} The 'MetaData' parameter cannot be used with 'Stream', 'Output', 'Filter', or 'ShowProgress'.`);
-
+        }
         if (Stream && Output) throw new Error(`${colors.red("@error:")} The 'Stream' parameter cannot be used with 'Output'.`);
 
         const EngineMeta: EngineOutput | null = await Agent({ Query, Verbose, UseTor });
 
-        if (!EngineMeta) {
-            throw new Error(`${colors.red("@error:")} Unable to retrieve a response from the engine.`);
-        }
-        // Access MetaData correctly from the new structure
-        if (!EngineMeta.MetaData) {
-            throw new Error(`${colors.red("@error:")} Metadata not found in the engine response.`);
-        }
+        if (!EngineMeta) throw new Error(`${colors.red("@error:")} Unable to retrieve a response from the engine.`);
+
+        if (!EngineMeta.MetaData) throw new Error(`${colors.red("@error:")} Metadata not found in the engine response.`);
 
         if (MetaData) {
             return {
