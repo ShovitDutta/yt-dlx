ef5425a20594331a684692f4edd7160f8dc738bb
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:48:05 +0530

    YouTubeDLX

--- context/content/src/routes/Audio/Lowest.ts
+++ context/content/src/routes/Audio/Lowest.ts
@@ -42,10 +42,7 @@ export default async function AudioLowest({
             throw new Error(`${colors.red("@error:")} The 'MetaData' parameter cannot be used with 'Stream', 'output', 'Filter', or 'ShowProgress'.`);
         }
         if (Stream && Output) throw new Error(`${colors.red("@error:")} The 'Stream' parameter cannot be used with 'output'.`);
-
         const EngineMeta: EngineOutput | null = await Agent({ Query: Query, Verbose: Verbose, UseTor: UseTor });
-        // Changed to Title Case
-
         if (!EngineMeta) {
             throw new Error(`${colors.red("@error:")} Unable to retrieve a response from the engine.`);
         }
