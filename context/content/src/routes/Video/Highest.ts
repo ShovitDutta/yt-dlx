4943a120ad9dc363f4e074cc4ac4034581c22ad2
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:56:18 +0530

    YouTubeDLX

--- context/content/src/routes/Video/Highest.ts
+++ context/content/src/routes/Video/Highest.ts
@@ -73,23 +73,17 @@ export default async function VideoHighest({
         const instance: ffmpeg.FfmpegCommand = ffmpeg();
 
         const paths = await locator();
-        if (!paths.ffmpeg) {
-            throw new Error(`${colors.red("@error:")} ffmpeg executable not found.`);
-        }
-        if (!paths.ffprobe) {
-            throw new Error(`${colors.red("@error:")} ffprobe executable not found.`);
-        }
+        if (!paths.ffmpeg) throw new Error(`${colors.red("@error:")} ffmpeg executable not found.`);
+
+        if (!paths.ffprobe) throw new Error(`${colors.red("@error:")} ffprobe executable not found.`);
+
         instance.setFfmpegPath(paths.ffmpeg);
         instance.setFfprobePath(paths.ffprobe);
-        if (EngineMeta.Thumbnails.Highest?.url) {
-            instance.addInput(EngineMeta.Thumbnails.Highest.url);
-        }
+        if (EngineMeta.Thumbnails.Highest?.url) instance.addInput(EngineMeta.Thumbnails.Highest.url);
 
-        // Get the highest quality video format from the Standard Dynamic Range category
         const highestVideo = EngineMeta.VideoOnly.Standard_Dynamic_Range.Highest;
-        if (!highestVideo || !highestVideo.url) {
-            throw new Error(`${colors.red("@error:")} Highest quality video URL not found.`);
-        }
+        if (!highestVideo || !highestVideo.url) throw new Error(`${colors.red("@error:")} Highest quality video URL not found.`);
+
         instance.addInput(highestVideo.url);
 
         instance.withOutputFormat("matroska");
