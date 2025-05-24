d6c4f9776979aaad8cabeef2a62c42564cf54ff6
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-24 20:45:03 +0530

    YouTubeDLX

--- context/content/src/routes/Audio/Highest.ts
+++ context/content/src/routes/Audio/Highest.ts
@@ -69,9 +69,7 @@ export default async function AudioHighest({
         const instance: ffmpeg.FfmpegCommand = ffmpeg();
         const paths = await locator();
         if (!paths.ffmpeg) throw new Error(`${colors.red("@error:")} ffmpeg executable not found.`);
-
         if (!paths.ffprobe) throw new Error(`${colors.red("@error:")} ffprobe executable not found.`);
-
         instance.setFfmpegPath(paths.ffmpeg);
         instance.setFfprobePath(paths.ffprobe);
         if (EngineMeta.Thumbnails.Highest?.url) instance.addInput(EngineMeta.Thumbnails.Highest.url);
