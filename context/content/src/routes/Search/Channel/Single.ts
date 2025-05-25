8832b118c8a884b2b68bcc84f495488d0bae6ce7
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-25 07:44:41 +0530

    YouTubeDLX

--- context/content/src/routes/Search/Channel/Single.ts
+++ context/content/src/routes/Search/Channel/Single.ts
@@ -7,13 +7,13 @@ export default async function channel_data({ ChannelLink, Verbose }: z.infer<typ
         ZodSchema.parse({ ChannelLink, Verbose });
         const youtube = new Client();
         const channelData: Channel | undefined = await youtube.getChannel(ChannelLink);
-        if (!channelData) throw new Error(`${colors.red("@error: ")} Unable to fetch channel data for the provided link.`);
+        if (!channelData) throw new Error(colors.red("@error: ") + ` Unable to fetch channel data for the provided link.`);
         return { data: channelData };
     } catch (error) {
-        if (error instanceof ZodError) throw new Error(`${colors.red("@error:")} Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`);
+        if (error instanceof ZodError) throw new Error(colors.red("@error:") + ` Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`);
         else if (error instanceof Error) throw error;
-        else throw new Error(`${colors.red("@error:")} An unexpected error occurred: ${String(error)}`);
+        else throw new Error(colors.red("@error:") + ` An unexpected error occurred: ${String(error)}`);
     } finally {
-        if (Verbose) console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
+        if (Verbose) console.log(colors.green("@info:"), `❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.`);
     }
 }
