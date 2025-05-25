d14c5e30f6af09badbe22b99e096c4f53c511538
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-25 07:55:17 +0530

    YouTubeDLX

--- context/content/src/routes/Misc/System/Help.ts
+++ context/content/src/routes/Misc/System/Help.ts
@@ -4,6 +4,6 @@ export default async function help(): Promise<string> {
         console.log(colors.green("@info:") + "❣️ Thank you for using " + colors.green("yt-dlx.") + " Consider " + colors.green("🌟starring") + " the GitHub repo " + colors.green("https://github.com/yt-dlx\n"));
         return colors.bold.white(`@help: visit https://yt-dlx.vercel.app`);
     } catch (error) {
-        throw new Error(colors.red("@error:") + ` An unexpected error occurred: ${error instanceof Error ? error.message : String(error)}`);
+        throw new Error(colors.red("@error:") + " An unexpected error occurred: " + (error instanceof Error ? error.message : String(error)));
     }
 }
