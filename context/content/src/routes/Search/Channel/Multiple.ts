abdf1b04bf6822fc06053cb9bc5a2c7c59a3c3d0
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-25 07:45:03 +0530

    YouTubeDLX

--- context/content/src/routes/Search/Channel/Multiple.ts
+++ context/content/src/routes/Search/Channel/Multiple.ts
@@ -22,20 +22,20 @@ async function searchChannels({ Query }: { Query: string }): Promise<channelSear
         }));
         return result;
     } catch (error: any) {
-        throw new Error(`${colors.red("@error: ")} ${error.message}`);
+        throw new Error(colors.red("@error: ") + ` ${error.message}`);
     }
 }
 export default async function search_channels({ Query, Verbose }: z.infer<typeof ZodSchema>): Promise<{ data: channelSearchType[] }> {
     try {
         ZodSchema.parse({ Query, Verbose });
         const channels = await searchChannels({ Query });
-        if (!channels || channels.length === 0) throw new Error(`${colors.red("@error: ")} No channels found for the provided Query.`);
+        if (!channels || channels.length === 0) throw new Error(colors.red("@error: ") + ` No channels found for the provided Query.`);
         return { data: channels };
     } catch (error) {
-        if (error instanceof ZodError) throw new Error(`${colors.red("@error:")} Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`);
+        if (error instanceof ZodError) throw new Error(colors.red("@error:") + ` Argument validation failed: ${error.errors.map(e => `${e.path.join(".")}: ${e.message}`).join(", ")}`);
         else if (error instanceof Error) throw error;
-        else throw new Error(`${colors.red("@error:")} An unexpected error occurred: ${String(error)}`);
+        else throw new Error(colors.red("@error:") + ` An unexpected error occurred: ${String(error)}`);
     } finally {
-        if (Verbose) console.log(colors.green("@info:"), "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.");
+        if (Verbose) console.log(colors.green("@info:"), `❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.`);
     }
 }
