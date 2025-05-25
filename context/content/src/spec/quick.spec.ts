8d0ebfca6bedb8ed1326bf00b2ce397cc1ca1b77
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-25 07:53:50 +0530

    YouTubeDLX

--- context/content/src/spec/quick.spec.ts
+++ context/content/src/spec/quick.spec.ts
@@ -6,5 +6,5 @@ console.clear();
 (async () => {
     const respEngine = await YouTubeDLX.Misc.Video.Extract({ Query: "https://www.youtube.com/watch?v=30LWjhZzg50" });
     fs.writeFileSync("Engine.json", JSON.stringify(respEngine, null, 2));
-    console.log("Completed quick test with YouTubeDLX" + "!");
+    console.log("Completed quick test with YouTubeDLX!");
 })().catch(console.error);
