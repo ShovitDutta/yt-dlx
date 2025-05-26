f6e2dfa3479efdfac6c9babb46aee115a6f2cbee
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-26 20:34:54 +0530

    YouTubeDLX

--- context/content/src/routes/Audio_Video/Highest.ts
+++ context/content/src/routes/Audio_Video/Highest.ts
@@ -68,7 +68,7 @@ type AudioVideoHighestOptions = z.infer<typeof ZodSchema>;
  * - For any FFmpeg-related errors during streaming or download: `Error: @error: FFmpeg Stream error: [error_message]` or `Error: @error: FFmpeg download error: [error_message]`
  * - For any other unexpected errors: `Error: @error: An unexpected error occurred: [error_message]`
  */
-export default async function AudioVideoHighest({ Query, Output, UseTor, Stream, Filter, MetaData, Verbose, ShowProgress, AudioLanguage }: AudioVideoHighestOptions): Promise<
+export default async function Audio_Video_Highest({ Query, Output, UseTor, Stream, Filter, MetaData, Verbose, ShowProgress, AudioLanguage }: AudioVideoHighestOptions): Promise<
     | {
           MetaData: EngineOutput["MetaData"];
           FileName: string;
