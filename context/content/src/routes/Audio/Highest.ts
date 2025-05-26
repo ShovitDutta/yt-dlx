19d47b79fbed0cd4633fcfc90591f9d06b9863eb
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-26 07:36:06 +0530

    YouTubeDLX

--- context/content/src/routes/Audio/Highest.ts
+++ context/content/src/routes/Audio/Highest.ts
@@ -28,10 +28,10 @@ export default async function AudioHighest({
     UseTor,
     Stream,
     Filter,
-    MetaData,
     Verbose,
-    ShowProgress,
     Language,
+    MetaData,
+    ShowProgress,
 }: AudioHighestOptions): Promise<{ MetaData: object } | { OutputPath: string } | { Stream: Readable; FileName: string }> {
     try {
         ZodSchema.parse({ Query, Output, UseTor, Stream, Filter, MetaData, Verbose, ShowProgress, Language });
